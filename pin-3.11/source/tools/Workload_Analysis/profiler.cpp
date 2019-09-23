78fe1329879c9cc944ea20fd8200418def3eaa4c
shihao-song <shihao.song@drexel.edu>
2019-09-23 04:40:46 -0400

    Framework is done.

--- pin-3.11/source/tools/Workload_Analysis/profiler.cpp
+++ pin-3.11/source/tools/Workload_Analysis/profiler.cpp
@@ -18,30 +18,116 @@
  * */
 
 static UINT64 insn_count = 0; // Track how many instructions we have already instrumented.
-MICRO_OP micro_op; // Container for the decoded instruction.
-// Function: Increment instruction count
-static void increICount()
+static void increCount() { ++insn_count; }
+
+// Function: branch predictor simulation
+static void bpSim(ADDRINT eip, BOOL taken, ADDRINT target)
 {
-    ++insn_count;
+    MICRO_OP micro_op;
+
+    micro_op.setPC(eip);
+    micro_op.setBranch();
+    micro_op.setTaken(taken);
+
+    // TODO, sending micro_op to a branch predictor
 }
-// Function: Prepare branch instruction.
-//static void prepBranch()
-//{
 
-//}
+// Function: memory access simulation
+static void memAccessSim(ADDRINT eip, bool is_store, ADDRINT mem_addr, UINT32 payload_size)
+{
+    MICRO_OP micro_op;
 
+    micro_op.setPC(eip);
+    if (is_store)
+    {
+        micro_op.setStore();
+    }
+    else
+    {
+        micro_op.setLoad();
+    }
+    micro_op.setMemAddr(mem_addr);
+
+    // TODO, send to a MMU simulator and a cache simulator
+    if (micro_op.isLoad())
+    {
+        printf("R\n");
+    }
+    else
+    {
+        printf("W\n");
+    }
+}
+
+// "Main" function: decode and simulate the instruction
 static void instructionSim(INS ins, VOID *v)
 {
+    // Step one, increment instruction count.
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)increCount, IARG_END);
 
-    // Step one, update the instruction count.
-    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)increICount, IARG_END);
-
-    // Step two, decoding instruction.
+    // Step two, decode and simulate instruction.
     if (INS_IsBranch(ins) && INS_HasFallThrough(ins))
     {
-        
-    }
+        // Why two calls for a branch?
+        // A branch has two path: a taken path and a fall-through path.
+        INS_InsertCall(
+            ins,
+            IPOINT_TAKEN_BRANCH, // Insert a call on the taken edge of the control-flow instruction
+            (AFUNPTR)bpSim,
+            IARG_ADDRINT, INS_Address(ins),
+            IARG_BOOL, TRUE,
+            IARG_BRANCH_TARGET_ADDR,
+            IARG_END);
 
+        INS_InsertCall(
+            ins,
+            IPOINT_AFTER, // Insert a call on the fall-through path of 
+                          // the last instruction of the instrumented object
+            (AFUNPTR)bpSim,
+            IARG_ADDRINT, INS_Address(ins),
+            IARG_BOOL, FALSE,
+            IARG_BRANCH_TARGET_ADDR,
+            IARG_END);
+    }
+    else if (INS_IsMemoryRead (ins) || INS_IsMemoryWrite (ins))
+    {
+        for (unsigned int i = 0; i < INS_MemoryOperandCount(ins); i++)
+        {
+            // Why predicated call?
+            // For conditional move, the memory access is only executed when predicated true.
+
+            // Why invoke twice?
+            // Note that in some architectures a single memory operand can be
+            // both read and written (for instance incl (%eax) on IA-32)
+            // In that case we instrument it once for read and once for write.
+
+            if (INS_MemoryOperandIsRead(ins, i))
+            {
+                INS_InsertPredicatedCall(
+                    ins,
+                    IPOINT_BEFORE,
+                    (AFUNPTR)memAccessSim,
+                    IARG_ADDRINT, INS_Address(ins),
+                    IARG_BOOL, FALSE,
+                    IARG_MEMORYOP_EA, i,
+                    IARG_UINT32, INS_MemoryOperandSize(ins, i),
+                    IARG_END);
+            }
+
+            if (INS_MemoryOperandIsWritten(ins, i))
+            {
+                INS_InsertPredicatedCall(
+                    ins,
+                    IPOINT_BEFORE,
+                    (AFUNPTR)memAccessSim,
+                    IARG_ADDRINT, INS_Address(ins),
+                    IARG_BOOL, TRUE,
+                    IARG_MEMORYOP_EA, i,
+                    IARG_UINT32, INS_MemoryOperandSize(ins, i),
+                    IARG_END);
+            }
+        }
+    }
 }
 
 static void printResults(int dummy, VOID *p)
--- pin-3.11/source/tools/Workload_Analysis/src/micro_op.hh
+++ pin-3.11/source/tools/Workload_Analysis/src/micro_op.hh
@@ -12,11 +12,11 @@ struct MICRO_OP
     Instruction_Type instr_type = Instruction_Type::MAX; // Instruction type
 
     Addr memory_addr; // load or store address (for LOAD and STORE instructions)
-    unsigned payload_size; // how much data (in bytes) to be loaded/stored.
+//    unsigned payload_size; // how much data (in bytes) to be loaded/stored.
 
     bool taken = false; // If the instruction is a branch, what is the real direction (not the predicted).
                         // You should reply on this field to determine the correctness of your predictions.
-    Addr branch_target_addr; // If the instruction is a branch, what is the branch target address. 
+//    Addr branch_target_addr; // If the instruction is a branch, what is the branch target address. 
 
     /* Member Functions */
     void setPC(Addr _PC) { PC = _PC; }
@@ -25,11 +25,11 @@ struct MICRO_OP
     void setEXE() { instr_type = Instruction_Type::EXE; }
     bool isEXE() { return instr_type == Instruction_Type::EXE; }
 
-    void setBRANCH() { instr_type = Instruction_Type::BRANCH; }
-    bool isBRANCH() { return instr_type == Instruction_Type::BRANCH; }
+    void setBranch() { instr_type = Instruction_Type::BRANCH; }
+    bool isBranch() { return instr_type == Instruction_Type::BRANCH; }
 
-    void setLOAD() { instr_type = Instruction_Type::LOAD; }
-    bool isLOAD() { return instr_type == Instruction_Type::LOAD; }
+    void setLoad() { instr_type = Instruction_Type::LOAD; }
+    bool isLoad() { return instr_type == Instruction_Type::LOAD; }
 
     void setStore() { instr_type = Instruction_Type::STORE; }
     bool isStore() { return instr_type == Instruction_Type::STORE; }
@@ -37,7 +37,7 @@ struct MICRO_OP
     void setMemAddr(Addr _addr) { memory_addr = _addr; }
     Addr getMemAddr() { return memory_addr; }
 
-    void setTaken() { taken = true; }
+    void setTaken(bool _taken) { taken = _taken; }
     bool isTaken() { return taken; }
 };
 
